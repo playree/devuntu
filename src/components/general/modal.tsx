7788153aaec3296aeb51c7744835ffe14a248f33
Kazuki Minakawa <kazuki.minakawa@funlab.jp>
2026-08-02 08:35:14 +0900

    ConfirmModalリファクタリング

--- src/components/general/modal.tsx
+++ src/components/general/modal.tsx
@@ -158,7 +158,7 @@ export const ConfirmModal = forwardRef<ConfirmModalRef, ConfirmModalParam>(({ ui
           </Modal.Header>
           <Modal.Body>
             <FlexCol>
-              <div className='text-foreground whitespace-pre-wrap'>{confirmParam?.text || ''}</div>
+              <div className='text-foreground wrap-anywhere whitespace-pre-wrap'>{confirmParam?.text || ''}</div>
               {confirmParam?.requireCheck && (
                 <Checkbox
                   id='confirm-agree'
