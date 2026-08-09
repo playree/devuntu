b81e6c18e5797d578e62d7456fe8163290ade9f7
Kazuki Minakawa <kazuki.minakawa@funlab.jp>
2026-08-09 19:30:18 +0900

    upgrade packages

--- src/components/general/cookie/types.ts
+++
@@ -1,9 +0,0 @@
-import { SerializeOptions } from 'cookie'
-import { IncomingMessage, ServerResponse } from 'http'
-
-export interface OptionsType extends SerializeOptions {
-  res?: ServerResponse
-  req?: IncomingMessage & {
-    cookies?: { [key: string]: string } | Partial<{ [key: string]: string }>
-  }
-}
