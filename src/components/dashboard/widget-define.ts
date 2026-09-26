103aefcd3e9350437b88e9ec8ce1dd16b64c61c1
Kazuki Minakawa <kazuki.minakawa@funlab.jp>
2026-09-26 11:03:55 +0900

    デフォルトレイアウト修正

--- src/components/dashboard/widget-define.ts
+++ src/components/dashboard/widget-define.ts
@@ -1,6 +1,6 @@
 import { DashboardLayout } from '@/lib/schema/schema-dashboard'
 
 export const WidgetDefaultLayout: DashboardLayout = {
-  left: ['my_tickets', 'app_info', 'server_info', null, null, null, null, null, null, null],
-  right: ['release_Note', null, null, null, null, null, null, null, null, null],
+  left: ['my_tickets', 'mentions', 'due_soon', null, null, null, null, null, null, null],
+  right: ['announcement', 'app_info', 'release_Note', null, null, null, null, null, null, null],
 } as const
